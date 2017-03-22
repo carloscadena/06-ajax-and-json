4647fb78db292823875eea37a9801036465d7809
Benjamin <ayzenbergb@gmail.com>
2017-03-21 17:00:37 -0700

    functional article load

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -50,9 +50,9 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(localStorage.rawData); //TODO: What do we need to pass in to Article.loadAll()?
+    Article.loadAll(localStorage.rawData); //DONE: What do we need to pass in to Article.loadAll()?
     //TODO: What method do we call to render the index page?
-    Article.toHtml();
+    articleView.initIndexPage();
 
   } else {
     // TODO: When we don't already have the rawData,
@@ -60,5 +60,11 @@ Article.fetchAll = function() {
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
+    $.getJSON("data/hackerIpsum.json")
+    .then(function(rawData) {
+      localStorage.rawData = rawData;
+      Article.loadAll(rawData);
+      articleView.initIndexPage();
+    });
   }
 }
